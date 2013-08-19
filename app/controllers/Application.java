46b82d65e3f6586c5725c784fd4d146a2e27d3de
Nemo HunJae Lee <nemo.hj.lee@gmail.com>
2013-08-19 16:48:26 +0900

    Index is slightly updated.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,19 @@ import play.mvc.*;
 
 import views.html.*;
 
+import java.util.*;
+
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        //return ok(index.render("Your new application is ready."));
+	StringBuffer sb = new StringBuffer();
+	sb.append("Use the following urls to give events and get events");
+	sb.append("\n");
+	sb.append("http://fresto1.owlab.com:9999/feedR0");
+	sb.append("\n");
+	sb.append("http://fresto1.owlab.com:9999/getR0");
+        return ok(sb.toString());
     }
   
 }
