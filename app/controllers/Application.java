1c7e43340ceab3219d40da9a41b37dd6c9eddc75
owlab <ernest@fresto1.owlab.com>
2013-08-26 16:30:08 +0900

    Small Change

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,7 +12,7 @@ public class Application extends Controller {
     public static Result index() {
         //return ok(index.render("Your new application is ready."));
 	StringBuffer sb = new StringBuffer();
-	sb.append("Use the following urls to give events and get events");
+	sb.append("Under construction. \n Urls to give or get events");
 	sb.append("\n");
 	sb.append("http://fresto1.owlab.com:9999/feedR0");
 	sb.append("\n");
