c28ad2001e49301c9e5f5074625bbd07319db208
owlab <hunjae.lee@gmail.com>
2013-11-01 05:51:22 +0900

    Add license text

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,3 +1,14 @@
+/**************************************************************************************
+ * Copyright 2013 TheSystemIdeas, Inc and Contributors. All rights reserved.          *
+ *                                                                                    *
+ *     https://github.com/owlab/fresto                                                *
+ *                                                                                    *
+ *                                                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * This file is licensed under the Apache License, Version 2.0 (the "License");       *
+ * you may not use this file except in compliance with the License.                   *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 * 
+ **************************************************************************************/
 package controllers;
 
 import play.*;
@@ -33,9 +44,6 @@ public class Application extends Controller {
 	StringBuffer sb = new StringBuffer();
 	sb.append("Under construction. \n Available urls:");
 	sb.append("\n");
-	sb.append("http://fresto1.owlab.com:9999/whatIsMyIPAddress");
-	sb.append("http://fresto1.owlab.com:9999/feedUIEvent");
-	sb.append("http://fresto1.owlab.com:9999/statistics");
 	//sb.append("\n");
 	//sb.append("http://fresto1.owlab.com:9999/getR0");
         return ok(sb.toString());
